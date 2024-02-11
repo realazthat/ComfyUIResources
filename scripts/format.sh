cd0ed9090ab23a62f94caa11ad2ace77493e6792
Azriel Fasten <realazthat@gmail.com>
2024-02-11 17:45:27 -0500

    Add formatting script.

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,9 +3,7 @@
 # https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
 set -e -x -v -u -o pipefail
 
-# source ~/.nvm/nvm.sh
-# nvm use
-# npm install
-# npx prettier --write "./site/content/**/*.md" --print-width 80 --prose-wrap always
 
 mdformat "./site/content/"
+mdformat README.md
+yapf -r -i src/
